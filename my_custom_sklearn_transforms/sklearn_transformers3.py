189fb752cbc7bbc17d1657358be3a43e31ca29e1
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-19 00:52:13 -0300

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers3.py
+++ my_custom_sklearn_transforms/sklearn_transformers3.py
@@ -1,9 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
-import sys
-old_syspath = sys.path
-sys.path = [path for path in sys.path if 'customspace' not in path]
-
 import pandas as pd
 
 class GetDummies(BaseEstimator, TransformerMixin):
@@ -11,16 +6,9 @@ class GetDummies(BaseEstimator, TransformerMixin):
         self.columns = None
         self.dummy_columns = dummy_columns
 
-    def fit_(self, X):
-    	return pd.get_dummies(X, columns=self.dummy_columns).columns
-
     def fit(self, X, y=None):
-        self.columns = self.fit_(X)
         return self
 
-    def transform_(self, X):
-    	return pd.get_dummies(X, columns=self.dummy_columns)
-
     def transform(self, X):
-        X_new = transform_(X)
+        X_new = pd.get_dummies(X)
         return X_new.reindex(columns=self.columns, fill_value=0)
