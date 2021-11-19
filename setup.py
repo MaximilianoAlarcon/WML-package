bc21d71845986a538404749ee2729885beac0741
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-19 00:40:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 
 
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
 setup(
       name='my_custom_sklearn_transforms',
       version='1.0',
@@ -10,6 +21,7 @@ setup(
       ''',
       url='https://github.com/MaximilianoAlarcon/WML-package/',
       author='Vanderlei Munhoz',
+      install_requires=load_requirements("requirements.txt"),
       author_email='vnderlev@protonmail.ch',
       license='BSD',
       packages=[
