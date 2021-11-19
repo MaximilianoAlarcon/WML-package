cc8f5506eb8604fc86248b3e8ebdcc6a963b0fd1
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-19 00:37:23 -0300

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers3.py
+++ my_custom_sklearn_transforms/sklearn_transformers3.py
@@ -1,23 +1,20 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-import numpy as np
 import pandas as pd
 
 class GetDummies(BaseEstimator, TransformerMixin):
-    def __init__(self, dummy_columns,pd):
+    def __init__(self, dummy_columns):
         self.columns = None
         self.dummy_columns = dummy_columns
-        self.pd = pd
 
     def fit_(self, X):
-    	return self.pd.get_dummies(X, columns=self.dummy_columns).columns
+    	return pd.get_dummies(X, columns=self.dummy_columns).columns
 
     def fit(self, X, y=None):
         self.columns = self.fit_(X)
         return self
 
     def transform_(self, X):
-    	return self.pd.get_dummies(X, columns=self.dummy_columns)
+    	return pd.get_dummies(X, columns=self.dummy_columns)
 
     def transform(self, X):
         X_new = transform_(X)
