6dec8f829092b1f8ccea1fa12856e82b14e40106
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-19 00:35:11 -0300

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers3.py
+++ my_custom_sklearn_transforms/sklearn_transformers3.py
@@ -4,19 +4,20 @@ import numpy as np
 import pandas as pd
 
 class GetDummies(BaseEstimator, TransformerMixin):
-    def __init__(self, dummy_columns):
+    def __init__(self, dummy_columns,pd):
         self.columns = None
         self.dummy_columns = dummy_columns
+        self.pd = pd
 
     def fit_(self, X):
-    	return pd.get_dummies(X, columns=self.dummy_columns).columns
+    	return self.pd.get_dummies(X, columns=self.dummy_columns).columns
 
     def fit(self, X, y=None):
         self.columns = self.fit_(X)
         return self
 
     def transform_(self, X):
-    	return pd.get_dummies(X, columns=self.dummy_columns)
+    	return self.pd.get_dummies(X, columns=self.dummy_columns)
 
     def transform(self, X):
         X_new = transform_(X)
