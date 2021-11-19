e7d32613b8e13fbfc7939dbf92720fbe782c2591
MaximilianoAlarcon <62962926+MaximilianoAlarcon@users.noreply.github.com>
2021-11-19 01:08:48 -0300

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers2.py
+++ my_custom_sklearn_transforms/sklearn_transformers2.py
@@ -1,6 +1,23 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 import numpy as np
+import pandas as pd
+
+
+class GetDummies(BaseEstimator, TransformerMixin):
+    def __init__(self, dummy_columns):
+        self.columns = None
+        self.dummy_columns = dummy_columns
+    def fit(self, X, y=None):
+        #self.columns = pandas.get_dummies(X, columns=self.dummy_columns).columns
+        return self
+    def transform(self, X):
+        #X_new = pandas.get_dummies(X)
+        #return X_new.reindex(columns=self.columns, fill_value=0)
+        return self
+
+
+
 
 # Un transformador para remover columnas indeseadas
 class Transform_data(BaseEstimator, TransformerMixin):
--- my_custom_sklearn_transforms/sklearn_transformers3.py
+++ my_custom_sklearn_transforms/sklearn_transformers3.py
@@ -6,11 +6,9 @@ class GetDummies(BaseEstimator, TransformerMixin):
     def __init__(self, dummy_columns):
         self.columns = None
         self.dummy_columns = dummy_columns
-
     def fit(self, X, y=None):
         #self.columns = pandas.get_dummies(X, columns=self.dummy_columns).columns
         return self
-
     def transform(self, X):
         #X_new = pandas.get_dummies(X)
         #return X_new.reindex(columns=self.columns, fill_value=0)
